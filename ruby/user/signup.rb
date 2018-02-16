ada8d94d17b7bd07686851d2f2eb0376d884c58f
Angel Young <angel.m.young24@gmail.com>
2018-02-16 17:26:12 -0500

    comment out liness 9 and 10

--- ruby/user/signup.rb
+++ ruby/user/signup.rb
@@ -5,6 +5,6 @@ end
 
 post '/creating_user' do
 
-  User.create(username: params[:username], password: params[:pass1] password: params[:pass2])
-  redirect '/profile'
+  # User.create(username: params[:username], password: params[:pass1], password: params[:pass2])
+  # redirect '/profile'
 end
